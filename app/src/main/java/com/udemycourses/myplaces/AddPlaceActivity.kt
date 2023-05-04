af872917a84a5b3c32e1d6f2a1cff4ae4b7c10e6
Vaggos B <37471733+VaggosBt@users.noreply.github.com>
2023-05-04 17:21:50 +0300

    Add Date with DatePickerDialog to AddPlaceActivity.kt and Add permissions request for access phone's Gallery (for either >=Android 11 and Android 10>= versions)

--- app/src/main/java/com/udemycourses/myplaces/AddPlaceActivity.kt
+++ app/src/main/java/com/udemycourses/myplaces/AddPlaceActivity.kt
@@ -1,23 +1,195 @@
 package com.udemycourses.myplaces
 
-import android.R
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.provider.Settings
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.udemycourses.myplaces.databinding.ActivityAddPlaceBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddPlaceActivity : AppCompatActivity() {
+class AddPlaceActivity : AppCompatActivity() , View.OnClickListener{
 
-    private var binding : ActivityAddPlaceBinding? = null
+    private var binding: ActivityAddPlaceBinding? = null
+    private val cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
+    private val REQUEST_READ_PERMISSION = 1
+
+    val openGalleryLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK && result.data != null) {
+                Toast.makeText(this@AddPlaceActivity, "PERMISSION GRANTED", Toast.LENGTH_SHORT).show()
+                //binding?.ivImage?.setImageResource(result.resultCode)
+            }
+        }
+
+    private val requestPermission : ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()){
+                permissions ->
+            permissions.entries.forEach{
+                val permissionName = it.key
+                val isGranted = it.value
+
+                if(isGranted){
+
+                    Toast.makeText(this, "Permission granted, now you can read the storage files", Toast.LENGTH_SHORT).show()
+                    val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    openGalleryLauncher.launch(pickIntent)
+                }else{
+                    if(permissionName == Manifest.permission.CAMERA){
+                        Toast.makeText(this, "Oops, you just denied the permission.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityAddPlaceBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
         setSupportActionBar(binding?.toolbarAddPlace)
-       supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         binding?.toolbarAddPlace?.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+
+        dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateInView()
+        }
+        binding?.etDate?.setOnClickListener(this)
+        binding?.tvAddImage?.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+
+        when (v!!.id) {
+            binding!!.etDate.id -> {
+                DatePickerDialog(
+                    this@AddPlaceActivity,
+                    dateSetListener, cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+            binding!!.tvAddImage.id -> {
+
+                val pictureDialog = AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+                val pictureDialogItems =
+                    arrayOf("Select photo from Gallery", "Capture photo from camera")
+                pictureDialog.setItems(pictureDialogItems) { dialog, choice ->
+                    when (choice) {
+
+                        0 -> choosePhotoFromGallery()
+
+
+                        1 -> Toast.makeText(
+                            this@AddPlaceActivity,
+                            "Camera selection coming soon...",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }.show()
+            }
+        }
+    }
+
+    private fun choosePhotoFromGallery() {
+
+        requestStoragePermission()
+
+    }
+
+
+    private fun updateDateInView() {
+        val myFormat = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        binding?.etDate?.setText(sdf.format(cal.time).toString())
+    }
+
+    private fun requestStoragePermission() {
+        // for android Android 11 or higher
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (Environment.isExternalStorageManager()) {
+                // Permission already granted
+                val pickIntent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                openGalleryLauncher.launch(pickIntent)
+            } else {
+                // Permission not granted, request it
+                val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+                Toast.makeText(
+                    this,
+                    "Please grant permission to access external storage.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        } else {
+            // for Android 10 or lower, request the permission
+            val permissions: Array<String>
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                permissions = arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            } else {
+                permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) ||
+                ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            ) {
+                showRationaleDialog(
+                    "Drawing App",
+                    "Drawing App needs to access your external storage"
+                )
+            } else {
+                requestPermission.launch(permissions)
+            }
+
+
+        }
+    }
+    private fun showRationaleDialog(
+        title: String,
+        message: String,
+    ){
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel"){dialog, _->
+                dialog.dismiss()
+            }
+        builder.create().show()
     }
 }
